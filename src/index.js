cb8747b140422cc4b9c953eb5789b8430765c6e0
Gabriel Guzman <gabriel.guzman@trinom.io>
2020-04-30 04:37:21 -0300

    gitignore

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+// styles
+import "./assets/css/bootstrap.min.css";
+// import "assets/scss/paper-kit.scss";
+import "./assets/css/paper-kit.css";
+import "./assets/demo/demo.css";
+// pages
+import Index from "./views/Index.js";
+// import NucleoIcons from "./views/NucleoIcons.js";
+// import LandingPage from "./views/examples/LandingPage.js";
+// import ProfilePage from "./views/examples/ProfilePage.js";
+// import RegisterPage from "./views/examples/RegisterPage.js";
+import SearchPage from "./pages/SearchPage";
+import ResultsPage from "./pages/ResultsPage";
+// others
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={props => <Index {...props} />} />
+      <Route path="/search" render={props => <SearchPage {...props} />} />
+      <Route path="/results" render={props => <ResultsPage {...props} />} />
+      {/* <Route
+        path="/nucleo-icons"
+        render={props => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={props => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={props => <ProfilePage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={props => <RegisterPage {...props} />}
+      /> */}
+      <Redirect to="/index" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
