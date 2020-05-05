a5fc3b5059080c37746f3d7b3811678184a9fb9d
Gabriel Guzman <gabriel.guzman@trinom.io>
2020-05-05 15:20:35 -0300

    Fix search if you press 'enter'
    * delete unused files

--- src/components/search/SearchBars.js
+++ src/components/search/SearchBars.js
@@ -10,6 +10,17 @@ import {
 import { withRouter } from 'react-router-dom';
 
 class SearchBars extends Component {
+  constructor(props) {
+    super(props)
+
+    this.keyPress = this.keyPress.bind(this);
+  }
+
+  keyPress(e){
+    if(e.keyCode === 13){
+      this.props.onClick();
+    }
+  }
 
 	render() {
 		return(
@@ -17,9 +28,16 @@ class SearchBars extends Component {
 					<Row>
             <Col className="offset-1 col-md-10 mt-4">
               <InputGroup addontype="append">
-                <Input className="form-control" placeholder="Search" type="text" value={this.props.value} onChange={this.props.onChange}/>
+                <Input
+                  className="form-control"
+                  placeholder="Search"
+                  type="text"
+                  value={this.props.value}
+                  onChange={this.props.onChange}
+                  onKeyDown={this.keyPress}
+                  />
                 <Button variant="outline-secondary" onClick={this.props.onClick}>
-                <i className="nc-icon nc-zoom-split" />
+                  <i className="nc-icon nc-zoom-split" />
                 </Button>
                 {/* resultados for "styrofoam"*/}
               </InputGroup>
